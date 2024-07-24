f41b19b4ffd444aa648e8369f4874504d3494f99
Enayar <rayane.g@proton.me>
2024-07-24 16:24:28 +0200

    process development
    initiate

---
+++ models/staging/stg_raw_ship.sql
@@ -0,0 +1,22 @@
+{{ config(materialized='view') }}
+
+with source as (
+
+    select * from {{ source('gz_raw_data', 'raw_gz_ship') }}
+
+),
+
+renamed as (
+
+    select
+        orders_id,
+        shipping_fee,
+        shipping_fee_1,
+        logcost,
+        ship_cost
+
+    from source
+
+)
+
+select * from renameds
